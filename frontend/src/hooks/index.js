a9cecf20288954ae316b2fefdc0606810f88a221
phessophissy <adefesobi@yahoo.com>
2026-01-20 21:16:07 +0100

    chore(hooks): update index to export all custom hooks

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,2 +1,9 @@
 // Hooks exports
 export * from './useContract';
+export { useFocusTrap, useAnnounce, useReducedMotion } from './useAccessibility';
+export { useLocalStorage, useSessionStorage } from './useLocalStorage';
+export { useDebounce, useThrottle, useDebouncedCallback } from './useDebounce';
+export { useIntersectionObserver, useLazyLoad, useInfiniteScroll } from './useIntersectionObserver';
+export { useMediaQuery, useBreakpoint, useWindowSize } from './useMediaQuery';
+export { useClipboard, useShare } from './useClipboard';
+export { useFetch, useMutation } from './useFetch';
