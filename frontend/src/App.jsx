99cae6631a496675950a3ab0912973db8ba5a96a
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-03-27 09:34:22 +0100

    docs: add JSDoc to App components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@
  * App component - updated at Fri Mar 27 09:34:22 2026
  */
 /**
+ * App component - updated at Fri Mar 27 09:34:22 2026
+ */
+/**
  * App component - updated at Fri Mar 27 09:32:39 2026
  */
 /**
