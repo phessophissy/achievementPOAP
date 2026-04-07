1fa8ef801e7774dffd284846f927ba9d6031fe8e
AchievementPOAP Dev <dev@achievementpoap.xyz>
2026-04-07 09:09:30 +0100

    mint: add utility stubs for feat/batch-mint feature

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -234,4 +234,4 @@ export default {
 // util helper PR-19: settings utilities
 // util helper PR-23: refactor utilities
 // util helper PR-26: gallery utilities
-// util helper PR-35: ui utilities
+// util helper PR-33: mint utilities
