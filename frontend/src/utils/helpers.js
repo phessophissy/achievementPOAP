8978bb87e3604b37ee8e97794ce129cc49624516
AchievementPOAP Dev <dev@achievementpoap.xyz>
2026-04-07 09:09:58 +0100

    ui: add utility stubs for fix/tooltip-overflow feature

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -234,4 +234,4 @@ export default {
 // util helper PR-19: settings utilities
 // util helper PR-23: refactor utilities
 // util helper PR-26: gallery utilities
-// util helper PR-36: profile utilities
+// util helper PR-35: ui utilities
