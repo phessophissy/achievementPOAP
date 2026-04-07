443e941f9f3a0554e392dd9129b119125f43a69d
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-07 10:48:50 +0100

    feat: polish NotFound page with animated illustration (#103)
    * pages: initialise PR-43 design token pass
    
    * pages: refine transition timing for PR-43
    
    * pages: add utility stubs for feat/404-page feature
    
    * docs: update README for feat/404-page
    
    * pages: extend CSS variables for feat/404-page
    
    * chore: add constants for feat/404-page feature
    
    * pages: add API helper stubs for feat/404-page
    
    * pages: add animation keyframe for feat/404-page
    
    * pages: export hook for feat/404-page functionality
    
    * docs: add contribution notes for feat/404-page
    
    ---------
    
    Co-authored-by: AchievementPOAP Dev <dev@achievementpoap.xyz>

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -235,4 +235,4 @@ export default {
 // util helper PR-23: refactor utilities
 // util helper PR-26: gallery utilities
 // util helper PR-37: ui utilities
-// util helper PR-44: utils utilities
+// util helper PR-43: pages utilities
