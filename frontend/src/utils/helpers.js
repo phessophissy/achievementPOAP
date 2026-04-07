59f176ba13efb2fcafdea4a3b92ab56f5e444455
AchievementPOAP Dev <dev@achievementpoap.xyz>
2026-04-07 09:07:59 +0100

    forms: add utility stubs for fix/input-validation feature

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -233,4 +233,4 @@ export default {
 // util helper PR-16: ux utilities
 // util helper PR-19: settings utilities
 // util helper PR-23: refactor utilities
-// util helper PR-27: home utilities
+// util helper PR-25: forms utilities
